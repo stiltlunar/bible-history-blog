f040f95e5a0bbd349582a000674abff230a4f962
Cody Meuer <93593334+stiltlunar@users.noreply.github.com>
2021-11-02 13:31:07 -0500

    dev landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import Layout from "../components/layout/layout"
 const IndexPage = () => {
   return (
     <Layout>
-
+      <h1>Website Under Development</h1>
+      <p>Come back soon for content</p>
     </Layout>
   )
 }
