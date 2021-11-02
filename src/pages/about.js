08b152a12544b9d0d78818c6584550d642f70cf6
Cody Meuer <93593334+stiltlunar@users.noreply.github.com>
2021-11-02 09:08:35 -0500

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return(
+    <main>
+      <h1>About Bible History</h1>
+      <p>This blog fulfills the requirements for a bible history course</p>
+    </main>
+  )
+}
+
+export default AboutPage;
